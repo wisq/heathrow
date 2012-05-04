dc876c02b0bed92af58f0ed772f08a9f3933f77a
Adrian Irving-Beer <adrian@shopify.com>
2012-05-04 16:02:24 -0400

    Add queue notification functions.

--- lib/heathrow/queue.rb
+++ lib/heathrow/queue.rb
@@ -25,10 +25,41 @@ class Heathrow::Queue
   end
 
   def next
-    Heathrow::Task.find(Heathrow.store.blpop(redis_key, 0).last)
+    store = Heathrow.store
+    store.del(current_key)
+    task = Heathrow::Task.find(Heathrow.store.blpop(redis_key, 0).last)
+    store.set(current_key, task.id)
+    task
   end
+
+  def notify_client(client, action, ahead = nil)
+    ahead ||= all_tasks.map(&:client).take_while { |c| c.id != client.id }.map(&:username).reverse
+    client << "You are ##{ahead.count} in line for #{action}.  (Ahead of you: #{ahead.join(', ')}.)"
+  end
+
+  def notify_pending(action)
+    current = Heathrow.store.get(current_key)
+    ahead = []
+
+    all_tasks.each do |task|
+      notify_client(task.client, action, ahead) unless task.id == current
+      ahead.unshift(task.client.username)
+    end
+  end
+
+  def all_tasks
+    current = Heathrow.store.get(current_key)
+    pending = Heathrow.store.lrange(redis_key, 0, -1)
+    users = ([current] + pending).compact.map { |id| Heathrow::Task.find(id) }
+  end
+
+  private
 
   def redis_key
     "queue:#{id}"
+  end
+
+  def current_key
+    "#{redis_key}:current"
   end
 end
--- test/unit/heathrow/queue_test.rb
+++ test/unit/heathrow/queue_test.rb
@@ -20,10 +20,46 @@ class QueueTest < TestHelper
     Heathrow::Queue.test_queue << task
   end
 
-  test "#next blocks and retrieves the next task" do
-    @store.expects(:blpop).with('queue:bundle_check', 0).returns(['queue:bundle_check', '123abc'])
-    Heathrow::Task.expects(:find).with('123abc').returns(task = mock)
+  test "#next clears current task, block-retrieves next task, and sets it as current" do
+    sequence = sequence('del_pop_set')
+    @store.expects(:del  ).in_sequence(sequence).with('queue:bundle_check:current')
+    @store.expects(:blpop).in_sequence(sequence).with('queue:bundle_check', 0).returns(['queue:bundle_check', '123abc'])
+    @store.expects(:set  ).in_sequence(sequence).with('queue:bundle_check:current', '123abc')
+    Heathrow::Task.expects(:find).with('123abc').returns(task = mock(:id => '123abc'))
 
     assert_equal task, Heathrow::Queue.bundle_check_queue.next
+  end
+
+  test "#notify_client sends message to selected client based on position" do
+    client1 = stub(:id => 'c1', :username => 'person1')
+    client2 = stub(:id => 'c2', :username => 'person2')
+    client3 = stub(:id => 'c3')
+    Heathrow::Task.expects(:find).with('123abc').returns(mock(:client => client1))
+    Heathrow::Task.expects(:find).with('456def').returns(mock(:client => client2))
+    Heathrow::Task.expects(:find).with('789fff').returns(mock(:client => client3))
+
+    @store.expects(:get).with('queue:bundle_check:current').returns('123abc')
+    @store.expects(:lrange).with('queue:bundle_check', 0, -1).returns(['456def', '789fff'])
+
+    client3.expects(:<<).with("You are #2 in line for bundle checking.  (Ahead of you: person2, person1.)")
+
+    Heathrow::Queue.bundle_check_queue.notify_client(client3, "bundle checking")
+  end
+
+  test "#notify_pending sends message to all waiting clients based on position" do
+    client1 = stub(:username => 'person1')
+    client2 = stub(:username => 'person2')
+    client3 = stub(:username => 'person3')
+    Heathrow::Task.expects(:find).with('123abc').returns(stub(:id => '123abc', :client => client1))
+    Heathrow::Task.expects(:find).with('456def').returns(stub(:id => '456def', :client => client2))
+    Heathrow::Task.expects(:find).with('789fff').returns(stub(:id => '789fff', :client => client3))
+
+    @store.expects(:get).with('queue:test:current').at_least_once.returns('123abc')
+    @store.expects(:lrange).with('queue:test', 0, -1).returns(['456def', '789fff'])
+
+    client1.expects(:<<).never
+    client2.expects(:<<).with("You are #1 in line for testing.  (Ahead of you: person1.)")
+    client3.expects(:<<).with("You are #2 in line for testing.  (Ahead of you: person2, person1.)")
+    Heathrow::Queue.test_queue.notify_pending("testing")
   end
 end
